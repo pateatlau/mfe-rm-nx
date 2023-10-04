92f3185d4ede762e201ca70b96e677cbd4ae87e4
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-10-04 16:53:06 +0530

    Minor change in cards app component

--- apps/cards/src/app/app.tsx
+++ apps/cards/src/app/app.tsx
@@ -1,11 +1,10 @@
 import { isEven } from '@mfe-rm-nx/utils';
 import { SmartCard } from '@mfe-rm-nx/shared-ui-components';
 
-
 export function App() {
   return (
     <div>
-      <h2>My Cards: Credit and Debit cards</h2>
+      <h2>Cards: Credit and Debit cards</h2>
       <SmartCard type="Credit" />
       <SmartCard type="Debit" />
       <p>10 is an {isEven(10) ? 'even ' : 'odd '} number</p>

8ca7a6025dba9acd3f7df4c6328a3f81d5464123
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-10-05 08:44:42 +0530

    isOdd function added in utils

--- libs/utils/src/lib/utils.spec.ts
+++ libs/utils/src/lib/utils.spec.ts
@@ -1,7 +1,11 @@
-import { isEven } from './utils';
+import { isEven, isOdd } from './utils';
 
 describe('utils', () => {
-  it('should work', () => {
+  it('should show 2 is an even number', () => {
     expect(isEven(2)).toBeTruthy;
+  });
+
+  it('should show 3 is an odd number', () => {
+    expect(isOdd(2)).toBeTruthy;
   });
 });
--- libs/utils/src/lib/utils.ts
+++ libs/utils/src/lib/utils.ts
@@ -1,3 +1,4 @@
 const isEven = (x: number): boolean => x % 2 === 0;
+const isOdd = (x: number): boolean => !isEven(x);
 
-export { isEven };
+export { isEven, isOdd };

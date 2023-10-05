9a282392a9fea9f0eada9f23b58fe8d409ec4c17
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-10-05 12:53:42 +0530

    Minor change made in shared-ui-components

--- libs/shared-ui-components/src/lib/smart-card.tsx
+++ libs/shared-ui-components/src/lib/smart-card.tsx
@@ -1,12 +1,17 @@
 import styles from './smart-card.module.css';
 
 /* eslint-disable-next-line */
-export interface SmartCardProps { type: string };
+export interface SmartCardProps {
+  type: string;
+}
 
 export function SmartCard(props: SmartCardProps) {
   return (
-    <div className={styles['container']} style={ {backgroundColor: props.type === 'Credit' ? 'gold' : 'black'} }>
-      My awesome {props.type} card
+    <div
+      className={styles['container']}
+      style={{ backgroundColor: props.type === 'Credit' ? 'gold' : 'black' }}
+    >
+      Awesome {props.type} card
     </div>
   );
 }

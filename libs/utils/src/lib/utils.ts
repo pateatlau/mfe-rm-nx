13e33ad08c777644fca016f31c56ca5481ee3502
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-10-09 09:11:21 +0530

    Minor change in Utils lib

--- libs/utils/src/lib/utils.ts
+++ libs/utils/src/lib/utils.ts
@@ -1,4 +1,4 @@
-const isEven = (x: number): boolean => x % 2 === 0;
+const isEven = (x: number): boolean => !(x % 2 !== 0);
 const isOdd = (x: number): boolean => !isEven(x);
 
 export { isEven, isOdd };

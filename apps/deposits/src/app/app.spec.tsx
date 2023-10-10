4d0cf7d8eb296577f668a682b5bf0a75a8e76ea6
Laldingliana Tlau Vantawl <pateatlau@gmail.com>
2023-10-10 09:22:47 +0530

    deposits unit test fixed

--- apps/deposits/src/app/app.spec.tsx
+++ apps/deposits/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/deposits/gi)).toBeTruthy();
+    expect(getByText(/Deposits at highest interest rates!/i)).toBeTruthy();
   });
 });
